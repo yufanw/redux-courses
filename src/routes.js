1c0a71f1ae2e749504071ff5e5a768a8b7f4e682
Yufan Wang <yufanwangcn@hotmail.com>
2018-04-24 16:23:50 -0700

    created react components, routing

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import App from './components/App.jsx';
-import HomePage from './components/home/HomePage.jsx';
-import AboutPage from './components/about/AboutPage.jsx';
+import App from './components/App';
+import HomePage from './components/home/HomePage';
+import AboutPage from './components/about/AboutPage';
+import CoursesPage from './components/course/CoursesPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="about" component={AboutPage} />
+    <Route path="courses" component={CoursesPage} />
   </Route>
 );
