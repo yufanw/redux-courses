182d188c6f39e81d6f48f9b6b039f54ac07d9054
Yufan Wang <yufanwangcn@hotmail.com>
2018-04-26 09:22:02 -0700

    added courses form

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,47 @@
 import React, {PropTypes} from 'react';
 
 class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: { title: '' }
+    };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(e) {
+    const course = this.state.course;
+    course.title = e.target.value;
+    this.setState({
+      course: course
+    });
+  }
+
+  onClickSave(e) {
+    const course = this.state.course;
+    course.title = '';
+    this.setState({
+      course: course
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        <input 
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}
+        />
+
+        <input 
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave}
+        />
       </div>
     );
   }
