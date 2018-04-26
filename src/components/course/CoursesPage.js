3d6f1691454dc3a60cec7cd0a23de5bb069fa0a0
Yufan Wang <yufanwangcn@hotmail.com>
2018-04-26 10:37:47 -0700

    implemented redux data flow

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,11 +1,14 @@
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
 
     this.state = {
-      course: { title: '' }
+      course: {title: ''}
     };
     this.onTitleChange = this.onTitleChange.bind(this);
     this.onClickSave = this.onClickSave.bind(this);
@@ -20,17 +23,19 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave(e) {
-    const course = this.state.course;
-    course.title = '';
-    this.setState({
-      course: course
-    });
+    this.props.actions.createCourse(this.state.course);
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
   }
 
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
+        <h2>Add Course</h2>
         <input 
           type="text"
           onChange={this.onTitleChange}
@@ -47,4 +52,21 @@ class CoursesPage extends React.Component {
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
 import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 render (
-  <Router history={browserHistory} routes={routes} />,
-    document.getElementById('app')
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.getElementById('app')
 );
