74f4afa9fe0ea24043f77068a88c25640320fda4
Yufan Wang <yufanwangcn@hotmail.com>
2018-04-27 11:36:47 -0700

    production build

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+/*eslint-disable import/default */
 import React from 'react';
 import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import {Provider} from 'react-redux';
 import {loadCourses} from './actions/courseActions';
 import {loadAuthors} from './actions/authorActions';
-import configureStore from './store/configureStore';
+import configureStore from './store/configureStore'; /*eslint-disable import/no-named-as-default */
 import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
