95dd331c83569dec7b3420b2266d9a90bb9ee404
Yufan Wang <yufanwangcn@hotmail.com>
2018-05-21 09:20:33 -0700

    dispatch api calls after render

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /*eslint-disable import/default */
 import React from 'react';
-import {render} from 'react-dom';
-import {Router, browserHistory} from 'react-router';
-import {Provider} from 'react-redux';
-import {loadCourses} from './actions/courseActions';
-import {loadAuthors} from './actions/authorActions';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { loadCourses } from './actions/courseActions';
+import { loadAuthors } from './actions/authorActions';
 import configureStore from './store/configureStore'; /*eslint-disable import/no-named-as-default */
 import routes from './routes';
 import './styles/styles.css';
@@ -12,12 +12,13 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/toastr/build/toastr.min.css';
 
 const store = configureStore();
-store.dispatch(loadCourses());
-store.dispatch(loadAuthors());
 
-render (
+render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
   </Provider>,
   document.getElementById('app')
 );
+
+store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
